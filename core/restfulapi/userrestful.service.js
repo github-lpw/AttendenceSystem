4bfda2f2857163122828ff69439ebee3eaff2d36
lpw <iwen_migd@163.com>
2016-11-30 08:27:08 +0800

    The function of refresh the local view

--- core/restfulapi/userrestful.service.js
+++ core/restfulapi/userrestful.service.js
@@ -1,6 +1,6 @@
 angular.module('core.userRestful')
 .factory('userRestful',['$resource',
   function($resource){
-    return $resource('http://172.23.229.86:3000/users/login',{loginname:'lpw',password:'123456'});
+    return $resource('http://172.23.197.191:3000/users/login',{loginname:'lpw',password:'123456'});
   }
 ]);
