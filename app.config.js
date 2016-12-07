34f7986d50911dc3296c7a4f0bd5f9bc52ade422
hhhouwt <hhhouwt@163.com>
2016-12-07 10:26:14 +0800

    hhhouwt modifty

--- app.config.js
+++ app.config.js
@@ -1,21 +1,23 @@
 'use strict';
 angular.module('attendenceSystem').
-config(['$locationProvider','$routeProvider','$httpProvider',
-  function config($locationProvider,$routeProvider,$httpProvider){
+config(['$locationProvider', '$routeProvider', '$httpProvider',
+    function config($locationProvider, $routeProvider, $httpProvider) {
 
-    $locationProvider.hashPrefix('!');
+        $locationProvider.hashPrefix('!');
 
-    $routeProvider.
-      when('/loginPage', {
-        template: '<login-page></login-page>',
-        css:'css/login.css'
-      }).
-      when('/mainPage/', {
-        template: '<main-page></main-page>',
-        css:['css/main-page/AdminLTE.min.css','css/main-page/_all-skins.min.css','css/main-page/skin-blue-light.min.css','css/main-page/skin-blue.min.css']
-      }).
-      otherwise('/loginPage');
+        $routeProvider.
+        when('/loginPage', {
+            template: '<login-page></login-page>',
+            css: 'css/login.css'
+        }).
+        when('/mainPage/', {
+            template: '<main-page></main-page>',
+            css: ['css/main-page/leaveRecords/dataTables.bootstrap.css', 'css/main-page/AdminLTE.min.css',
+                'css/main-page/_all-skins.min.css', 'css/main-page/skin-blue-light.min.css', 'css/main-page/skin-blue.min.css'
+            ]
+        }).
+        otherwise('/loginPage');
 
-  }
+    }
 
 ]);
